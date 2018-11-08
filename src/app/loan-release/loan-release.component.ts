434e869996476e5e8274ae5aabc071ae6658f57b
suicodave <suico.dave@gmail.com>
2018-11-08 15:22:56 +0800

    feat(loan release) added csv conversion

--- src/app/loan-release/loan-release.component.ts
+++ src/app/loan-release/loan-release.component.ts
@@ -1,26 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
+import { LoanReleaseService } from '../services/loan-release.service';
+import { Subscription } from 'rxjs';
+import { Papa } from 'ngx-papaparse';
 
 @Component({
   selector: 'app-loan-release',
   templateUrl: './loan-release.component.html',
   styleUrls: ['./loan-release.component.scss']
 })
-export class LoanReleaseComponent implements OnInit {
+export class LoanReleaseComponent implements OnInit, OnDestroy {
   isLoading = false;
-  constructor(private snackBar: MatSnackBar) { }
+  loanServiceSubscription: Subscription;
+  constructor(private snackBar: MatSnackBar, private loanReleaseService: LoanReleaseService, private csvParser: Papa) { }
 
   ngOnInit() {
   }
 
   generate() {
     this.isLoading = true;
+
+    this.loanServiceSubscription = this.loanReleaseService.fetch().subscribe(
+      (res: any) => {
+        this.downloadData(res.data);
+      }
+    );
     setTimeout(() => {
       this.isLoading = false;
       this.snackBar.open('Report has been exported successfully.', 'Ok', {
         duration: 5000
       });
     }, 3000);
+  }
+
+  downloadData(data) {
+    const csvReport = this.csvParser.unparse(data);
+    const blob = new Blob([csvReport], { type: 'text/csv;charset=utf-8' });
+  }
+
+  ngOnDestroy() {
+    // tslint:disable-next-line:triple-equals
+    if (this.loanServiceSubscription != undefined) {
+      this.loanServiceSubscription.unsubscribe();
+    }
   }
 
 }
--- src/app/services/loan-release.service.ts
+++ src/app/services/loan-release.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { apiUrl } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoanReleaseService {
+  private apiUrl = apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  fetch() {
+    const url = `${this.apiUrl}loan-releases`;
+    return this.http.get(url);
+  }
 }
